ae30ce2bce1d9414fb178295a6b240c77aa93083
kevindaniel <kevindaniel@192.168.1.71>
2015-12-12 08:47:28 -0600

    LEER COMENTARIO

--- ITCHLabs/src/itchlabs/Alumnos.java
+++ ITCHLabs/src/itchlabs/Alumnos.java
@@ -41,7 +41,7 @@ public class Alumnos extends javax.swing.JFrame {
         am = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setText("No_Control:");
 
